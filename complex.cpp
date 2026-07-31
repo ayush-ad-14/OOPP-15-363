05ec7c991a9e472916d5413c6beed7034bba2026
Ayush Anand <ayushabhi18@gmail.com>
2026-07-31 10:18:48 +0530

    Wrote add function for complex numbers

--- complex.cpp
+++ complex.cpp
@@ -22,16 +22,41 @@ public:
             cout << img << "i" << endl;
         }
     }
+    void add(Complex p) {
+        real = real + p.real;
+        img = img + p.img;
+    }
+   
+    void addi(int a , int b) {
+        real = real + a;
+        img = img + b;
+    }
+    void addit(Complex p,Complex q) {
+        real = p.real + q.real;
+        img = p.img + q.img;
+    }
+     Complex additi(int a , int b) {
+        Complex m;
+        m.real = real + 5;
+        m.img = img + 10;
+        return m;
+    }
 };
 
-int main()
-{
-    Complex c1, c2;
+int main() {
+    Complex c1,c2,c3;
     c1.input();
     c2.input();
-    // c1.add();
     c1.show();
     c2.show();
-
+    c1.add(c2);
+    c1.show();
+    c2.add(c1);
+    c1.show();
+    c2.addi(5 ,2);
+    c1.addi(9 , 8);
+    c3.addit(c1,c2);
+    c3.show();
+    c1.additi(5,10);
     return 0;
 }
