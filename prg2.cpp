b9615a43a83a761077d73e1f661b3db41c69d45d
Ayush Anand <ayushabhi18@gmail.com>
2026-07-31 09:36:17 +0530

    adasds

---
+++ prg2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+// Call by Value
+void swapValue(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "Inside Call by Value: ";
+    cout << "a = " << a << " b = " << b << endl;
+}
+
+// Call by Reference
+void swapReference(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "Inside Call by Reference: ";
+    cout << "a = " << a << " b = " << b << endl;
+}
+
+// Call by Address
+void swapAddress(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+
+    cout << "Inside Call by Address: ";
+    cout << "*a = " << *a << " *b = " << *b << endl;
+}
+
+int main()
+{
+    int x = 10, y = 20;
+
+    cout << "Original Values: ";
+    cout << "x = " << x << " y = " << y << endl;
+
+    // Call by Value
+    swapValue(x, y);
+    cout << "After Call by Value: ";
+    cout << "x = " << x << " y = " << y << endl;
+
+    // Call by Reference
+    swapReference(x, y);
+    cout << "After Call by Reference: ";
+    cout << "x = " << x << " y = " << y << endl;
+
+    // Call by Address
+    swapAddress(&x, &y);
+    cout << "After Call by Address: ";
+    cout << "x = " << x << " y = " << y << endl;
+
+    return 0;
+}
